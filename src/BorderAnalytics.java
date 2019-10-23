0641a7b46bf298947cd7b13e118739a87b74e953
Ha Nguyen <nguyenttha89@gmail.com>
2019-10-23 12:50:54 -0400

    code, files

--- src/BorderAnalytics.java
+++ src/BorderAnalytics.java
@@ -14,120 +14,158 @@ import java.io.File;
 
 public class BorderAnalytics {
 
+    private static String input_file = "";
+    private static String output_file = "";
     private static Scanner scan;
 
     /**
      * The main function is where the file will execute first. It calls other functions in the file
      */
     public static void main(String[] args) {
-        String input_file = "";
-        String output_file= "";
+//        Read the argument from the execution file, in which the first and second args are the directory of input and output file, respectively
         for (int i = 0; i < args.length; i++) {
             input_file = args[0];
             output_file = args[1];
         }
-        calValueAndAvg(readData(input_file), output_file);
+        writeData(calValueAndAvg(readData()));
     }
 
+
     /**
-     * This function reads each row from the .csv file and then each row will be created as an object and saved in the list
+     * This function checks the valid file and then reads each row from the .csv file and then each row will be created as an
+     * object and saved in the list
+     *
      * @return list of data
      */
 
-    public static List<DataEntry> readData(String input_file) {
-//        Create the list to store data from the file as we have many rows
+    public static List<DataEntry> readData() {
+//      Create the list to store data from the file as we have many rows
         List<DataEntry> list = new ArrayList<DataEntry>();
         int i = 0;
         try {
+//          Read the input file
             scan = new Scanner(new File(input_file));
-            while (scan.hasNext()) {
-//                The first line which is header is not read. It starts reading from the second line
-                if (i == 0) {
-                    scan.nextLine();
-                    i++;
-                } else {
-//                    Data in each row is separated by the comma as we want to obtain each value in the row
-                    String[] data = scan.nextLine().split(",");
-//                    Each row is stored in the object as it's easier for us to handle and retrieve data from object. This process will
-//                    continue to the end of the file
-                    DataEntry data_entry = new DataEntry();
-                    data_entry.setBorder(data[3]);
-                    data_entry.setDate(data[4]);
-                    data_entry.setMeasure(data[5]);
-                    data_entry.setValue(Integer.parseInt(data[6]));
-                    list.add(data_entry);
+            {
+                while (scan.hasNext()) {
+                    if (i == 0) {
+//                  Check the header data. If Border, Measure, Date, and Value are not in the right place, inform an error message.
+//                  If it's correct, we go to the next line to obtain data.
+                        String[] header_data = scan.nextLine().split(",");
+                        if (header_data[3].equalsIgnoreCase("Border") && header_data[4].equalsIgnoreCase("Date") &&
+                                header_data[5].equalsIgnoreCase("Measure") && header_data[6].equalsIgnoreCase("Value")) {
+                            i++;
+                        } else {
+                            throw new IOException("The file is incorrect format");
+                        }
+                    } else {
+//                      This line below gets each value of each row by splitting the comma
+                        String[] data = scan.nextLine().split(",");
+                        // Each row is stored in the object as it's easier for us to handle and retrieve data from object. This process
+                        // will continue to the end of the file
+                        DataEntry data_entry = new DataEntry();
+                        data_entry.setBorder(data[3]);
+                        data_entry.setDate(data[4]);
+                        data_entry.setMeasure(data[5]);
+                        data_entry.setValue(Integer.parseInt(data[6]));
+//                      Once each row is added into DataEntry, it will be added into the list as an object
+                        list.add(data_entry);
+                    }
                 }
             }
-
+//          This code checks if the list is null, which means there is no data. Then, an error message is thrown
+            if (list.size() == 0) {
+                throw new Exception("No data is found");
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         } finally {
-//            At the end, it is important to close the stream as it may cause some unexpected issues if it is still opened
+//          At the end, it is important to close the stream as it may cause some unexpected issues if it is still opened
             scan.close();
         }
-//        Once we obtain the list of data, then this list is transferred to the next function to execute the calculation
+//      Once we obtain the list of data, then this list is transferred to the next function to execute the calculation
         return list;
     }
 
     /**
-     * This function is the most important one as it calculates the value and average. It receives the list result from the readData() and
-     * then goes through each object to retrieve data needed to the calculation
-     * @param list
+     * This function is the most important one as it calculates the value and average. It receives the data_entry_list result
+     * from the readData() and then goes through each object to retrieve data needed to the calculation. First, it calculates
+     * the Value of each object in the list. Then, it starts calculating the average based on this result
+     *
+     * @param data_entry_list
      */
 
-    public static void calValueAndAvg(List<DataEntry> list, String output_file) {
-//        In order to calculate the value, three important keys are border, date, and measure. Therefore, the program will run faster
-//        if we do not iterate
+    public static List calValueAndAvg(List<DataEntry> data_entry_list) {
+//      In order to calculate the value, three important keys are border, date, and measure. Therefore, the HashMap is created to store
+//      this set of keys. The list of DataExport objects is also needed to store and export data
         HashMap<String, Integer> map = new HashMap<String, Integer>();
-        List<DataExport> ed = new ArrayList<>();
-        DataExport export_data;
+        List<DataExport> data_export_list = new ArrayList<>();
+        DataExport data_export;
 
-        for (DataEntry item : list) {
-            String key = item.getBorder() + "," + item.getDate() + "," + item.getMeasure();
-            if (!map.containsKey(key)) {
-                map.put(key, item.getValue());
+//      This code goes through each item of the list and compare the key, which is the combination of border, date, and measure, to sum
+//      up the value of all objects having the same key. The HashMap then stores the updated data
+        for (DataEntry item : data_entry_list) {
+            String value_key = item.getBorder() + "," + item.getDate() + "," + item.getMeasure();
+            if (!map.containsKey(value_key)) {
+                map.put(value_key, item.getValue());
             } else {
-                map.replace(key, item.getValue() + map.get(key));
+                map.replace(value_key, item.getValue() + map.get(value_key));
             }
         }
 
+//      Iterate each item in the HashMap to save it into DataExport object.
         Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry<String, Integer> data = it.next();
-            export_data = new DataExport();
+            data_export = new DataExport();
             String[] keys = data.getKey().split(",");
-            export_data.setBorder(keys[0]);
-            export_data.setDate(keys[1]);
-            export_data.setMeasure(keys[2]);
-            export_data.setValue(data.getValue());
-            ed.add(export_data);
-            Collections.sort(ed, new DataExportComparator());
-            Collections.reverse(ed);
+            data_export.setBorder(keys[0]);
+            data_export.setDate(keys[1]);
+            data_export.setMeasure(keys[2]);
+            data_export.setValue(data.getValue());
+            data_export_list.add(data_export);
+//          This code is used to sort the list in descending order based on date, value, measure, and border. The sort function is
+//          defined in the DataExport file
+            Collections.sort(data_export_list, new DataExportComparator());
+            Collections.reverse(data_export_list);
         }
 
-        for(int i = 0; i < ed.size(); i++) {
+//      This code iterates through the list of DataExport objects to calculate the avg for each one and update it back to this object
+        for (int i = 0; i < data_export_list.size(); i++) {
             int month = 0;
             float totalValue = 0;
-            for(int j = i + 1; j < ed.size(); j ++) {
-                if(ed.get(i).getBorder().equals(ed.get(j).getBorder()) && ed.get(i).getMeasure().equals(ed.get(j).getMeasure())) {
+//          As the avg is calculated based on value from all previous month. Each object will compare with the rest of the list.
+//          Since data is already sorted, only border and measure needs to be compared. If any objects having the same border and measure,
+//          then, the value will be added to the current one and number of month is increased
+            for (int j = i + 1; j < data_export_list.size(); j++) {
+                if (data_export_list.get(i).getBorder().equals(data_export_list.get(j).getBorder()) && data_export_list.get(i).getMeasure().equals(data_export_list.get(j).getMeasure())) {
                     month += 1;
-                    totalValue += ed.get(j).getValue();
+                    totalValue += data_export_list.get(j).getValue();
                 }
             }
-            if(month != 0) {
-                int avg = (int)Math.ceil(totalValue / month);
-                ed.get(i).setAverage(avg);
+//          If no month is found, it means this record is unique, then the avg is still 0. Otherwise, the avg will be calculated by the
+//          formula below. Once the result is obtained, the avg of this record will be updated.
+            if (month != 0) {
+                int avg = (int) Math.ceil(totalValue / month);
+                data_export_list.get(i).setAverage(avg);
             }
         }
-        writeData(ed, output_file);
+//      Return the final set along with the results to the next function to write data
+        return data_export_list;
     }
 
-    public static void writeData(List<DataExport> list, String output_file) {
+    /**
+     * This function is used to write data from the list of DataExport objects obtained from the calValueAndAvg function
+     *
+     * @param data_export_list
+     */
+
+    public static void writeData(List<DataExport> data_export_list) {
         FileWriter csvWriter = null;
         try {
             csvWriter = new FileWriter(output_file);
+//          Write the header
             csvWriter.append("Border");
             csvWriter.append(",");
             csvWriter.append("Date");
@@ -139,20 +177,24 @@ public class BorderAnalytics {
             csvWriter.append("Average");
             csvWriter.append("\n");
 
-            for(DataExport item : list) {
+//          Write each DataExport object by iterating through the list obtained from the function above
+            for (DataExport item : data_export_list) {
                 csvWriter.append(item.toString());
                 csvWriter.append("\n");
             }
-        }catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
+//              At the end, it is important to flush and close the stream as it may cause some unexpected issues if it is still opened
                 csvWriter.flush();
                 csvWriter.close();
-            }catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
 }
+
+
--- src/DataExport.java
+++ src/DataExport.java
@@ -2,6 +2,10 @@ import java.text.SimpleDateFormat;
 import java.util.Comparator;
 import java.util.Date;
 
+/**
+ * This class is used to create an object to export data to the output file. This includes getter, setter, and toString() methods
+ */
+
 public class DataExport {
 
     private String border;
@@ -43,6 +47,7 @@ public class DataExport {
         this.measure = measure;
     }
 
+//  This function is where we define the desired format for each row of the output file
     @Override
     public String toString() {
         return border + "," + date + "," + measure + "," + value + "," + average;
@@ -58,11 +63,20 @@ public class DataExport {
 
 }
 
+/**
+ * This class is used to define sorting methods for the list of DataExport objects.
+ */
 
 class DataExportComparator implements Comparator<DataExport> {
 
     private static final String DATE_FORMAT = "dd/M/yyyy hh:mm:ss a";
 
+    /**
+     * This function receives 2 DataExport objects and perform the sorting in descending order on these objects
+     * @param o1
+     * @param o2
+     * @return the ascending order
+     */
     @Override
     public int compare(DataExport o1, DataExport o2) {
         Date date1 = null;
@@ -82,15 +96,20 @@ class DataExportComparator implements Comparator<DataExport> {
         int value_compare = o1.getValue().compareTo(o2.getValue());
         int measure_compare = o1.getMeasure().compareTo(o2.getMeasure());
         int border_compare = o1.getBorder().compareTo(o2.getBorder());
+//      It first compares date. If Date is equal, it then compares Value. The process keeps repeating until reaching Border
         if(date_compare == 0) {
             return ((value_compare == 0) ? ((measure_compare == 0) ? border_compare : measure_compare) : value_compare);
         } else {
             return date_compare;
         }
-
-
     }
 
+    /**
+     * This function converts the format of date from String to Date format as defined in the first line of this class
+     * @param date
+     * @return Date
+     * @throws Exception
+     */
     public Date convertDate(String date) throws Exception {
         Date d = new SimpleDateFormat(DATE_FORMAT).parse(date);
         return d;
